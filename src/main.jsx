a9c0684d3b0b2277e3748dbbea394ae0ff459e60
rajvedant64 <rajvedant64@gmail.com>
2025-08-22 19:17:47 +0530

    Cleaned main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './app.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
